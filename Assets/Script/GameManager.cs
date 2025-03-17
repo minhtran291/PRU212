36584a5ac117e0c1ece1369562cda9203453e112
Nguyễn Thịnh <nguyenthinhatmsma@gmail.com>
2025-03-17 13:32:30 +0700

    push ThinhsMap

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int populationStart = 5;
+    [SerializeField] private int woodStart = 100;
+    [SerializeField] private int meatStart = 100;
+    [SerializeField] private int stoneStart = 100;
+    [SerializeField] private int goldStart = 0;
+
+    public TextMeshProUGUI populationText;
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI meatText;
+    public TextMeshProUGUI stoneText;
+    public TextMeshProUGUI goldText;
+
+    public int stone;
+    public int gold;
+    public int population;
+    public int wood;
+    public int meat;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        population = populationStart;
+        wood = woodStart;
+        meat = meatStart;
+        stone = stoneStart;
+        gold = goldStart;
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        populationText.text = population.ToString();
+        woodText.text = wood.ToString();
+        meatText.text = meat.ToString();
+        stoneText.text = stone.ToString();
+        goldText.text = gold.ToString();
+    }
+
+    public void SpendStoneForTowerLv1(int amount)
+    {
+        if (stone >= amount)
+        {
+            stone -= amount;
+            UpdateUI();
+            Debug.Log("Xây! Số đá còn lại: " + stone);
+        }
+        else
+        {
+            Debug.Log("Không đủ đá để xây!");
+        }
+    }
+}
---
+++ Assets/Script/TowerPlace.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TowerPlacementManager : MonoBehaviour
+{
+    public GameObject towerPrefab;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main; 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero); 
+
+            if (hit.collider != null && hit.collider.CompareTag("TowerPlace")) 
+            {
+                Vector3 placePosition = hit.collider.bounds.center;
+                PlaceTower(placePosition);
+            }
+        }
+    }
+
+    private void PlaceTower(Vector3 position)
+    {
+        if (GameManager.Instance != null && GameManager.Instance.stone >= 50)
+        {
+            GameManager.Instance.SpendStoneForTowerLv1(50);
+            Instantiate(towerPrefab, position, Quaternion.identity);
+            Debug.Log("Tháp đã được đặt tại: " + position);
+        }
+        else
+        {
+            Debug.Log("Không đủ tài nguyên đá.");
+        }
+    }
+}
